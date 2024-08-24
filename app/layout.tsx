b60c1da35a7f8c77cad499e0aca2e2878a4512f3
Hemmyevo <xxxlildevil999@gmail.com>
2024-08-24 10:11:25 +0100

    fin

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-          <ConvexClientProvider>
+             <ConvexClientProvider>
           <TooltipProvider>
           {children}
           </TooltipProvider>
