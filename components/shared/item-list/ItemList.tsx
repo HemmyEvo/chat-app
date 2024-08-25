3ec61787ebdce79636558f82ca4abce8f5524ccd
Hemmyevo <xxxlildevil999@gmail.com>
2024-08-25 16:47:40 +0100

    item 2

--- components/shared/item-list/ItemList.tsx
+++ components/shared/item-list/ItemList.tsx
@@ -13,7 +13,7 @@ type Props = PropsWithChildren<{
 const ItemList = ({children, title, action:Action}: Props) => {
     const {isActive} = useConversation();
   return (
-   <Card className={cn("hidden overflow-hidden h-full w-full lg:w-80 lg:flex-none p-2", {
+   <Card className={cn("hidden overflow-scroll no-scrollbar h-full w-full lg:w-80 lg:flex-none p-2", {
         block : !isActive,
         "lg:block" : isActive,
    })}>
